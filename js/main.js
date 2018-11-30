6979b7fc4fc942484b262867294b0165c3672e9d
Peter Costa <petersvc@outlook.com>
2018-11-30 20:18:04 -0300

    fetch languages

--- js/main.js
+++ js/main.js
@@ -9,32 +9,30 @@ const avatarContainer = document.querySelector(".avatar")
 
 // pegando as informações do perfil do usuário. ex: nome, email, avatar
 const fetchUser = user => {
-    let endpoint = `https://api.github.com/users/${user}` // chamando da api
-    const data = fetch(endpoint).then(res => res.json()) // guardando a informação em um json             
+    let endpoint = `https://api.github.com/users/${user}` // chamando a api
+    let data = fetch(endpoint).then(res => res.json()) // guardando a informação em um json             
     return data                  
 }
 // informações sobre os repositórios do usuário. ex: nome, url das linguagens usadas nele
 const fetchRepo = user => {
     let endpoint = `https://api.github.com/users/${user}/repos`
-    const data = fetch(endpoint).then(res => res.json()) 
+    let data = fetch(endpoint).then(res => res.json()) 
     return data
 }
 // linguagens usadas no repositórios
-const fetchLang = lang => {
-    let endpoint = lang // array com as urls das linguagens, obtido na fetchRepo()
-    let data = [] // array que conterá as linguaguens de cada repositório
+const fetchLang = async lang => {
+    let endpoint = await lang // array com as urls das linguagens, obtido na fetchRepo()
+    let data = await [] // array que conterá as linguaguens de cada repositório
     for (let url of endpoint) {
         fetch(url)
             .then(res => res.json())
-            .then(res => data.push(res))                            
+            .then(res => data.push(res)) // inserindo o json de cada url de linguagens em um indice do array                           
     }
-    console.log(data)
     return data
 }
-
+// callbacks de usuário, repositórios e linguagens
 const showData = () => {
-    // User data
-    fetchUser(searchInput.value).then(res => {
+    fetchUser(searchInput.value).then(res => { // usuário
         let need = res.name + res.bio + res.company
         avatarContainer.src = `${res.avatar_url}`
         nameContainer.innerHTML = `${res.name}`
@@ -44,17 +42,21 @@ const showData = () => {
         else
             companyContainer.innerHTML = res.bio
 
-        locationContainer.innerHTML = `<i class="fas fa-map-marker-alt mr-1"></i>${res.location}`
+        locationContainer.innerHTML = `<i class="fas fa-map-marker-alt mr-1"></i>${res.location}`        
+    })
+    // repositórios e linguagens
+    fetchRepo(searchInput.value) // repositórios
+        .then(res => {
+            let repo_name = res.map(repo => repo.name)
+            let lang = res.map(names => names.languages_url)
+            // linguagens 
+            fetchLang(lang).then(res => {
+                console.log(lang, res)
+            })             
+        })
         
-    })
-
-    // Repositories data
-    fetchRepo(searchInput.value).then(res => {
-        let repo_name = res.map(repo => repo.name)
-        let lang = res.map(names => names.languages_url)
-        fetchLang(lang)
-    })
-}
+        
+    }   
 
 document.addEventListener("keyup", (event) => {
     if (event.key == 'Enter')
