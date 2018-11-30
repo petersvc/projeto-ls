8a1adc29bf593ccbdd7ee4e1b4c491b5c2c51980
Peter Costa <petersvc@outlook.com>
2018-11-30 16:44:36 -0300

    fetch languages

--- js/main.js
+++ js/main.js
@@ -1,27 +1,58 @@
-const inputValue = document.querySelector("#search")
+const searchInput = document.querySelector("#name")
 const searchBtn = document.querySelector(".searchButton")
-const nameContainer = document.querySelector(".main__profile-name")
-const unContainer = document.querySelector(".main__profile-username")
-const reposContainer = document.querySelector(".main__profile-repos")
-const urlContainer = document.querySelector(".main__profile-url")
+const nameContainer = document.querySelector(".git-name")
+const companyContainer = document.querySelector(".git-company")
+const locationContainer = document.querySelector(".git-location")
 const avatarContainer = document.querySelector(".avatar")
 
-const fetcher = async (user) => {
-    const api_call = await fetch(`https://api.github.com/users/${user}`)
-    const data = await api_call.json()
-    return {data}
+// fetchers: fazendo requisiçoes a api e retornando .json dos endpoints
+
+// pegando as informações do perfil do usuário. ex: nome, email, avatar
+const fetchUser = user => {
+    let endpoint = `https://api.github.com/users/${user}` // chamando da api
+    const data = fetch(endpoint).then(res => res.json()) // guardando a informação em um json             
+    return data                  
+}
+// informações sobre os repositórios do usuário. ex: nome, url das linguagens usadas nele
+const fetchRepo = user => {
+    let endpoint = `https://api.github.com/users/${user}/repos`
+    const data = fetch(endpoint).then(res => res.json()) 
+    return data
+}
+// linguagens usadas no repositórios
+const fetchLang = lang => {
+    let endpoint = lang // array com as urls das linguagens, obtido na fetchRepo()
+    let data = [] // array que conterá as linguaguens de cada repositório
+    for (let url of endpoint) {
+        fetch(url)
+            .then(res => res.json())
+            .then(res => data.push(res))                            
+    }
+    console.log(data)
+    return data
 }
 
 const showData = () => {
-    fetcher(inputValue.value).then((res) => {
-        console.log(res)
-        avatarContainer.src = `${res.data.avatar_url}`
-        nameContainer.innerHTML = `<span class="main__profile-value">Nome: ${res.data.name}</span>`
-        unContainer.innerHTML = `<span class="main__profile-value">Login: ${res.data.login}</span>`
-        reposContainer.innerHTML = `<span class="main__profile-value">Repositórios: ${res.data.public_repos}</span>`
-        urlContainer.innerHTML = `<span class="main__profile-value">Url: ${res.data.html_url}</span>`
-        //nameContainer.innerHTML = `<span class="main__profile-value">Nome: ${res.data[0].name}</span>`
+    // User data
+    fetchUser(searchInput.value).then(res => {
+        let need = res.name + res.bio + res.company
+        avatarContainer.src = `${res.avatar_url}`
+        nameContainer.innerHTML = `${res.name}`
 
+        if (res.bio == null) 
+            companyContainer.innerHTML = res.company
+        else
+            companyContainer.innerHTML = res.bio
+
+        locationContainer.innerHTML = `<i class="fas fa-map-marker-alt mr-1"></i>${res.location}`
+        
+    })
+
+    // Repositories data
+    fetchRepo(searchInput.value).then(res => {
+        let repo_name = res.map(repo => repo.name)
+        let lang = res.map(names => names.languages_url)
+        fetchLang(lang)
     })
 }
 
@@ -29,8 +60,9 @@ document.addEventListener("keyup", (event) => {
     if (event.key == 'Enter')
         showData()
 })
-searchBtn.addEventListener("click", () => {
+/*searchBtn.addEventListener("click", () => {
     showData()
-})
+})*/
 
 //https://www.youtube.com/watch?v=sJspH620ZsU&t=1408s
+//Make like a tree, leaves
---
+++ js/tests.js
@@ -0,0 +1,2 @@
+let a = [1,5,2,3]
+console.log(a.filter(value => value % 2 != 0))
