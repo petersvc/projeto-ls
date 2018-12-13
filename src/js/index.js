2cc6c46387b3a2e87bc4efd666d8371faa76bc81
Peter Costa <petersvc@outlook.com>
2018-12-13 11:51:58 -0300

    home almost done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { fetchUser, fetchRepo, fetchLang, langSum } from './api'
 import '../css/main.css'
-import '../css/bootstrap.min.css'
+import '../css/reset.css'
 
 //const searchBtn = document.querySelector(".searchButton")
 
@@ -52,12 +52,55 @@ $('.user-input').keyup( (event) => {
         showData()
     }
 })
+
+if ($('.first').css('display') != 'none')
+    $('.s1').toggleClass('toggler-focus')
+    $('.c1').css('background-color', 'rgba(34,34,51,1)')
+
 //searchBtn.addEventListener("click", () => { showData() })
 
 $('.fa-bars').click( () => {
     $('.nav-dev').toggleClass('nav-dev-toggle')
 })
 
+for (let i = 1; i < 6; i++)
+    $('.container').append(`<span class="bg-dash d${i}"></span>`)
 
-//https://www.youtube.com/watch?v=sJspH620ZsU&t=1408s
-//Make like a tree, leaves
+for (let i = 0; i < 13; i++){
+    if (i % 4 == 0)
+        $('.footer__dashes').append(`<li class="dashes__long"></li>`)
+    else
+        $('.footer__dashes').append(`<li class="dashes__short"></li>`)
+
+}
+
+
+/*
+
+https://www.youtube.com/watch?v=sJspH620ZsU&t=1408s
+Make like a tree, leaves
+
+
+
+.home-footer {
+  margin-top: 33%;
+  margin-left: 0;
+  padding: 0;
+  height: 35px;
+  background-color: rgba(28,28,45,1);
+  color: rgba(255,255,255,0.2);
+  text-align: center;
+}
+
+.home-footer .col-4 {
+  height: 100%;
+}
+.home-footer span {
+  line-height: 38px;
+  vertical-align: middle;
+}
+.toggler-focus {
+  border-top: 2px solid rgb(236, 85, 85);
+  color: rgba(255,255,255,1);
+}
+*/
