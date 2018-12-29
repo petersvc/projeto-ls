12b694555a252bb2f79ec442821c4cae4a4ced30
peter <petersvc@outlook.com>
2018-12-29 12:20:02 -0300

    del

--- src/js/api.js
+++ src/js/api.js
@@ -1,21 +1,20 @@
 export { fetchUser, fetchRepo, fetchLang }
 
 const apiUrl = 'https://api.github.com/users/' // url da api do github
-const token = '' // necessário para fazer mais de 60 requisições/hora
 
 // fetchers: fazem requisiçoes a api e retornam jsons
 
-const fetchUser = async user => { // função que extrai os dados do ${user}. Ex: user.name
+const fetchUser = async (user, token) => { // função que extrai os dados do ${user}. Ex: user.name
     const endpoint = apiUrl + user + token // guarda a url que contém os dados. Ex: ...github.com/users/${user}          
     return await fetch(endpoint).then(user => user.json()) // requisita os dados e os retorna extraídos em um .json                 
 }
 
-const fetchRepo = async repos_url => { // ...extrai os dados dos repositórios (repos) do ${user}
+const fetchRepo = async (repos_url, token) => { // ...extrai os dados dos repositórios (repos) do ${user}
     const endpoint = repos_url + token // ...Ex: ...com/users/user/${repos_url}
     return await fetch(endpoint).then(repos => repos.json()) 
 }
 
-const fetchLang = async validsRepos => { // ... extrai as informações das urls das linguagens
+const fetchLang = async (validsRepos, token) => { // ... extrai as informações das urls das linguagens
     let jsons = [] // guarda os jsons extraídos das urls
     let langsUrls = validsRepos.map(repo => repo.languages_url) // guarda as urls que contém os dados
     for (let url of langsUrls) { // percorre as urls
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
 import { fetchUser, fetchRepo, fetchLang } from './api'
 import { langSum, langArray, numberSort, stringSort, shortByte, langAcronym } from './aux'
+import { tk } from '../../token'
 import '../css/main.css'
 import '../css/reset.css'
 
-const showData = () => {    
-    fetchUser( $('.search__name').val() ).then(user => { // usuário
+const showData = () => {
+    let token = '' // necessário para fazer mais de 60 requisições/hora 
+    if (typeof tk == 'function')
+        token = tk()
+   
+    fetchUser($('.search__name').val(), token).then(user => { // usuário
         $('.user__avatar').attr('src', user.avatar_url)  // imagem do usuário
 
         if (user.name != null)
@@ -12,22 +17,18 @@ const showData = () => {
         else
             $('.user__name').html(user.login)
         
-        fetchRepo(user.repos_url).then(repos => { // respositórios
+        fetchRepo(user.repos_url, token).then(repos => { // respositórios
             const noForkeds = repos.filter(repo => repo.fork != true)            
             const forkeds = repos.length - noForkeds.length      
             const validsRepos = noForkeds.filter(repo => repo.language !== null)
-            console.log(`Repos: ${repos.length}\nNo forkeds: ${noForkeds.length}`)
-            console.log(`Forkeds: ${forkeds}\nValids: ${validsRepos.length}`)
             
-            fetchLang(validsRepos).then(langsJsons => { // linguagens
+            fetchLang(validsRepos, token).then(langsJsons => { // linguagens
                 langSum(langsJsons).then(langResult => { // retorna o total de bytes escritos em cada linguagem                    
                     $('.data__lang').remove()
                     $('.rank__percent').remove()
                     
                     const langSortNumber = langResult.slice(0).sort(numberSort)
-                    //const langSortString = langResult.slice(0).sort(stringSort)
-
-                    const la3 = langArray(langSortNumber)
+                    const la = langArray(langSortNumber)
 
                     let sb = 0
                     let divId = ''
@@ -35,7 +36,7 @@ const showData = () => {
                     let sizeWidth = 0
                     let sizeId = ''
 
-                    la3.map(langIndex => {
+                    la.map(langIndex => {
                         if (langIndex[2] > 0) {
                             sb = shortByte(langIndex[1])
 
@@ -78,109 +79,10 @@ $('.search__name').keyup( (event) => {
     }
 })
 
-//searchBtn.addEventListener("click", () => { showData() })
-
-$('.header__logotype h5').click( () => {
-    $('.first').show()
-    $('.second').hide()
-})
-
-$('.header__menu').click( () => {
-    $('.first').hide()
-    $('.second').show()
-})
-
-$('.yes').click( () => {
-    $('.second__check').hide()
-    $('.second__search').show()
-})
-
 for (let i = 0; i < 5; i++){
     $('.stats__separator').append(`<div class="graph__separator" id="separator${i}"></div>`)
 }
 
-/*
-for (let i = 0; i < 64; i++) {
-    if (i % 6 == 0)
-        $('.content__dots').append(`<div class="dots__dot dot-green" id="dot${i}"><i class="fas fa-square"></i></div>`)
-    else if (i % 5 == 0)
-        $('.content__dots').append(`<div class="dots__dot" id="dot${i}"><i class="fas fa-square"></i></div>`)
-    else if (i % 4 == 0)
-        $('.content__dots').append(`<div class="dots__dot" id="dot${i}"><i class="fas fa-square"></i></div>`)
-    else
-        $('.content__dots').append(`<div class="dots__dot dot-red" id="dot${i}"><i class="fas fa-circle"></i></div>`)
-
-}
-*/
-
-var mouse = {'x': 0, 'y': 0};
-
-let homex
-let homey
-let forcex
-let forcey
-let magnet
-let position
-let x0 
-let y0 
-let x1 
-let y1 
-let distancex
-let distancey
-let distance
-let powerx 
-let powery
-        
-
-homex = 0;
-homey = 0;
-forcex = 0;
-forcey = 0;
-magnet = 500;
-
-$(document).bind('mousemove', function(e) {
-    mouse = {'x': e.pageX, 'y': e.pageY};
-});
-
-
-$('.dots__dot').each(function(index, el){
-    $(el).data('homex', parseInt($(el).position().left));
-    $(el).data('homey', parseInt($(el).position().top));
-});
-
-$('.dots__dot').css('position','absolute');
-
-setInterval(function () {
-    $('.dots__dot').each(function(index, el){
-        el = $(el);
-        position = el.position();
-        x0 = el.offset().left;
-        y0 = el.offset().top;
-        x1 = mouse.x;
-        y1 = mouse.y;
-        distancex = x1-x0;
-        distancey = y1-y0;
-
-        distance = Math.sqrt((distancex * distancex) + (distancey * distancey));
-        
-        
-        magnet = 2600 - distance*20;
-        if(distance>130) {
-           magnet=0; 
-        }
-        
-        
-        powerx = x0 - (distancex / distance) * magnet / distance;
-        powery = y0 - (distancey / distance) * magnet / distance;
-        
-        forcex = (forcex + (el.data('homex') - x0) / 2) / 2.1;
-        forcey = (forcey + (el.data('homey') - y0) / 2) / 2.1;
-                    
-
-        el.css('left', powerx + forcex);
-        el.css('top',  powery + forcey);
-    });
-}, 15);
 
 /*
 https://www.youtube.com/watch?v=sJspH620ZsU&t=1408s
