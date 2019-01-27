a12e631a0d4a87e70d0c074301dccf77c32f226a
Peter Costa <petersvc@outlook.com>
2019-01-27 05:53:32 -0300

    repos per page fixed

--- src/js/api.js
+++ src/js/api.js
@@ -5,12 +5,13 @@ const apiUrl = 'https://api.github.com/users/' // url da api do github
 // fetchers: fazem requisiçoes a api e retornam jsons
 
 const fetchUser = async (user, token) => { // função que extrai os dados do ${user}. Ex: user.name
-    const endpoint = apiUrl + user + token // guarda a url que contém os dados. Ex: ...github.com/users/${user}          
+    const endpoint = apiUrl + user + '?' + token // guarda a url que contém os dados. Ex: ...github.com/users/${user}          
     return await fetch(endpoint).then(user => user.json()) // requisita os dados e os retorna extraídos em um .json                 
 }
 
 const fetchRepo = async (repos_url, token) => { // ...extrai os dados dos repositórios (repos) do ${user}
-    const endpoint = repos_url + token // ...Ex: ...com/users/user/${repos_url}
+    const endpoint = repos_url+'?per_page=300&' + token // ...Ex: ...com/users/user/${repos_url}
+    console.log(endpoint)
     $('bottom__repos .loader').hide(0)
     return await fetch(endpoint).then(repos => repos.json()) 
 }
@@ -20,7 +21,7 @@ const fetchLang = async (validsRepos, token) => { // ... extrai as informações
     let langsUrls = validsRepos.map(repo => repo.languages_url) // guarda as urls que contém os dados
     
     for (let url of langsUrls) { // percorre as urls
-        let json = await fetch(url + token).then(langs => langs.json()) // requista os dados, extrai em um .json e o guarda  
+        let json = await fetch(url + '?' + token).then(langs => langs.json()) // requista os dados, extrai em um .json e o guarda  
         jsons.push(json) // insere o .json no array de jsons
     }
 
